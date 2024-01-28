efafdafbd0b794782e8f9f8d38cf159bcf273060
fyap898 <124294533+fyap898@users.noreply.github.com>
2024-01-27 18:29:33 -0700

    input and macro define

---
+++ types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+//Code for goods constraints
+#define XD No_Dairy
+#define XF No_Frozen
+#define XM No_Meat
+#define XP No_Produce
+
+//Code for goods' type
+#define D Dairy
+#define F Frozen
+#define M Meat
+#define P Produce
+
+#endif
