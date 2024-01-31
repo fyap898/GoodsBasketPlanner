5d40106b14ccf4eea534340db6195376344b37f0
fyap898 <124294533+fyap898@users.noreply.github.com>
2024-01-30 22:58:55 -0700

    menu and adjust struct def

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <fstream>
-#include <types.h>
-
+#include "types.h"
 using namespace std;
+
+//Description: Prompts menu
+void menu_prompt();
+
+int main (){
+
+    menu_prompt();
+
+    return 0;
+}
+
+void menu_prompt()
+{
+    string infile;
+    cout << "Welcome to Goods Basket Planner\n\n";
+    cout << "Enter Info (file.txt)\n";
+    cin >> infile;
+    
+    
+
+    return;
+}
--- types.h
+++ types.h
@@ -1,7 +1,9 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <iostream>
 #include <string>
+using namespace std;
 
 //Code for goods constraints
 #define XD No_Dairy
@@ -23,16 +25,16 @@ typedef struct
     int item_size;
     int item_count;
     string item_constraint;
-}grocery_item;
+}g_item;
 
 typedef struct 
 {
-    string bag_type;
+    char bag_type;
     int bag_weight_limit;
     int bag_size_limit;
     int bag_count;
     string bag_constraints;
-}grocery_bag;
+}g_bag;
 
 
 #endif
