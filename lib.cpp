3159c46c54bbfdfba1bc8812d9fe161f8e431c51
fyap898 <124294533+fyap898@users.noreply.github.com>
2024-02-11 16:04:29 -0700

    .

--- lib.cpp
+++ lib.cpp
@@ -42,6 +42,7 @@ void read_item_datafile(g_item item[], int& item_fill, string item_data_file)
                         item[item_fill].item_size >>
                         item[item_fill].item_constraint || item_fill > MAX_ITEM)
         {
+            cout << "1\n";
             item_fill++;
         }
         infile.close();
@@ -87,7 +88,56 @@ int menu()
 
 void view_basket_content(g_basket basket[], int index)
 {
+    cout << "\nDisplaying all basket's content...\n"
+        << "-------------------------------------------------\n";
+
+    for(int i = 0; i < basket[i].fillLvl; i++)
+    {
+        cout << "Basket Index: " << basket[i].basket_index << "\n"
+             << "Weight Limit: " << basket[i].basket_weight_limit << "\n"
+             << "Size Limit: " << basket[i].basket_size_limit << "\n";
+
+        if(basket[i].basket_type == 'D')
+        {
+            cout << "Type: Dairy\n";
+        } else if(basket[i].basket_type == 'F')
+        {
+            cout << "Type: Frozen\n";
+        } else if(basket[i].basket_type == 'M')
+        {
+            cout << "Type: Meat\n";
+        } else if(basket[i].basket_type == 'P')
+        {
+            cout << "Type: Produce\n";
+        }
+        
+        if(basket[i].basket_constraints == "XD")
+        {
+            cout << "Constraint: " << basket[i].basket_constraints << "\n\n";
+        } else if(basket[i].basket_constraints == "XF")
+        {
+            cout << "Constraint: " << basket[i].basket_constraints << "\n\n";
+        } else if(basket[i].basket_constraints == "XM")
+        {
+            cout << "Constraint: " << basket[i].basket_constraints << "\n\n";
+        } else if(basket[i].basket_constraints == "XP")
+        {
+            cout << "Constraint: " << basket[i].basket_constraints << "\n\n";
+        }
     
+        cout << "Items: \n"
+             << "No. | Type\t| Weight | Size | Constraint\n" 
+             << "-------------------------------------------------\n";
+
+        for(int j = 0; j < basket[i].fillLvl; j++)
+        {
+            cout << basket[i].item_in_basket[j].item_index
+                 << basket[i].item_in_basket[j].item_type
+                 << basket[i].item_in_basket[j].item_weight
+                 << basket[i].item_in_basket[j].item_size
+                 << basket[i].item_in_basket[j].item_constraint << "\n";
+        }
+    }
 }
 
 void add_item(g_basket basket_array[], g_item item_array[])
